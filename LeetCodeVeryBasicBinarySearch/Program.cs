5a004a34bdd7de58f02fba21ee59124276dc6af3
VictorKithinji <victorkithinji@outlook.com>
2023-02-05 23:03:49 +0300

    Add project files.

---
+++ LeetCodeVeryBasicBinarySearch/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCodeVeryBasicBinarySearch;
+Console.WriteLine("Hello, World!");
+
+int[] testData = { 1, 0, 3, 5, 9, 12 };
+int testTarget = 9;
+
+SearchForTarget getTarget = new SearchForTarget();
+Console.WriteLine(getTarget.search(testData, testTarget));
---
+++ LeetCodeVeryBasicBinarySearch/SearchForTarget.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeVeryBasicBinarySearch
+{
+    internal class SearchForTarget
+    {
+        public int search(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int min = 0;
+            int max = nums.Length - 1;
+
+            if (target == null || nums.Length == 0 ) { return -1; }
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if(target < nums[mid])
+                {
+                    max= mid - 1;
+                }
+                else { min= mid + 1; }
+            }
+            return -1;
+        }
+    }
+}
